413af5b8b83dcffe672fae8d075a5825853d044b
cassiofiorin <fiorin.cassio@gmail.com>
2018-02-17 16:11:42 -0200

    Upload do site original

---
+++ Junkempo_arquivos/myScript.js
@@ -0,0 +1,16 @@
+﻿function scrollToID(myID) {
+
+    myID = '#' + myID;
+
+    var y = $(myID).offset().top + $('#frameParallax').scrollTop();
+
+        $('#frameParallax').animate({
+            scrollTop: y - 50
+        }, 800);
+}
+
+$(document).ready(function () {
+
+    
+});
+
